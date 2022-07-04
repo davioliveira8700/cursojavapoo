9b151e05fe8d46be601b08fa9d3802341b783aec
davioliveira8700 <odavi278@gmail.com>
2022-07-04 10:26:56 -0300

    Lista de discplinas

--- CursoJavaOO/src/cursojavaexecutavel/classes/Aluno.java
+++ CursoJavaOO/src/cursojavaexecutavel/classes/Aluno.java
@@ -1,5 +1,8 @@
 package cursojavaexecutavel.classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Aluno {
 
 	private String nome;
@@ -13,77 +16,16 @@ public class Aluno {
 	private String nomeEscola;
 	private String serieMatriculado;
 	
-	private String disciplina1;
-	private double nota1;
-	private String disciplina2;
-	private double nota2;
-	private String disciplina3;
-	private double nota3;
-	private String disciplina4;
-	private double nota4;
+	private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
+	
+	
 
-	public String getDisciplina1() {
-		return disciplina1;
+	public List<Disciplina> getDisciplinas() {
+		return disciplinas;
 	}
 
-	public void setDisciplina1(String disciplina1) {
-		this.disciplina1 = disciplina1;
-	}
-
-	public String getDisciplina2() {
-		return disciplina2;
-	}
-
-	public void setDisciplina2(String disciplina2) {
-		this.disciplina2 = disciplina2;
-	}
-
-	public String getDisciplina3() {
-		return disciplina3;
-	}
-
-	public void setDisciplina3(String disciplina3) {
-		this.disciplina3 = disciplina3;
-	}
-
-	public String getDisciplina4() {
-		return disciplina4;
-	}
-
-	public void setDisciplina4(String disciplina4) {
-		this.disciplina4 = disciplina4;
-	}
-
-	public double getNota1() {
-		return nota1;
-	}
-
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-
-	public double getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-
-	public double getNota3() {
-		return nota3;
-	}
-
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-
-	public double getNota4() {
-		return nota4;
-	}
-
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
+	public void setDisciplinas(List<Disciplina> disciplinas) {
+		this.disciplinas = disciplinas;
 	}
 
 	public void setNome(String nome) {
@@ -165,10 +107,15 @@ public class Aluno {
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
-
 	/* Metodo que retorna a media do aluno */
 	public double getMediaNota() {
-		return (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		double somaNotas = 0.0;
+		for(Disciplina disciplina : disciplinas) {
+			somaNotas += disciplina.getNota();
+		}
+		
+		return somaNotas / disciplinas.size();
 	}
 
 	public boolean getAprovado() {
@@ -179,7 +126,6 @@ public class Aluno {
 			return false;
 		}
 	}
-	
 	public String getAprovado1() {
 		double media = getMediaNota();
 		if(media >=70) {
@@ -190,6 +136,7 @@ public class Aluno {
 		
 	}
 
+
 	
 
 	@Override
@@ -197,9 +144,7 @@ public class Aluno {
 		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
 				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
 				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
-				+ serieMatriculado + ", disciplina1=" + disciplina1 + ", nota1=" + nota1 + ", disciplina2="
-				+ disciplina2 + ", nota2=" + nota2 + ", disciplina3=" + disciplina3 + ", nota3=" + nota3
-				+ ", disciplina4=" + disciplina4 + ", nota4=" + nota4 + "]";
+				+ serieMatriculado +  "]";
 	}
 
 	@Override
